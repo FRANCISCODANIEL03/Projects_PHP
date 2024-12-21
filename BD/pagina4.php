42f46d4a750e3665aaa1c47906238b339895f3fa
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-20 21:42:59 -0600

    add connection to db

--- BD/pagina4.php
+++ BD/pagina4.php
@@ -3,6 +3,11 @@
         <title>Problema</title>
     </head>
     <body>
-        
+        <?php
+        $conexion = mysqli_connect('host', 'user', 'password', 'base')
+        or die('Problemas con la conexion');
+
+      
+        ?>
     </body>
 </html>
