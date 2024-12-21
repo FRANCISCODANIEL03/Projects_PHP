06e2277282a2239fec9388bf5dc0c560a85f1772
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-20 21:37:00 -0600

    add html structure

---
+++ BD/pagina3.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Problema</title>
+    </head>
+    <body>
+        <form action="pagina4.php" method="post">
+            Ingrese el nombre del curso: 
+            <input type="text" name="nombrecurso" size="40"><br>
+            <input type="submit" value="Confirmar">
+        </form>
+    </body>
+</html>
