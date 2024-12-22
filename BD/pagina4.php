03a1135a67bf466de35823733e4b869f16c28581
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-21 21:43:30 -0600

    insert data to the db

--- BD/pagina4.php
+++ BD/pagina4.php
@@ -7,7 +7,11 @@
         $conexion = mysqli_connect('host', 'user', 'password', 'base')
         or die('Problemas con la conexion');
 
-      
+        mysqli_query($conexion, "INSERT cursos(nombrecurso) VALUES
+                    ('$_REQUEST[nombrecurso]')") 
+        or die('Prolemas en el select'.mysqli_error($conexion));
+
+       
         ?>
     </body>
 </html>
