f75455c12e6c0f7abbe8adde23d66607fc838f76
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-22 21:45:55 -0600

    show data from the db

--- BD/pagina5.php
+++ BD/pagina5.php
@@ -10,8 +10,26 @@
         $registros = mysqli_query($conexion, "SELECT codigo, nombre, mail, codigocurso FROM alumnos") 
         or die('Prolemas en el select'.mysqli_error($conexion));
 
-       
-        mysqli_close($conexion);
+        while($reg = mysqli_fetch_array($registros)){
+            echo 'Codigo: '. $reg['codigo']. '<br>';
+            echo 'Nombre: '. $reg['nombre']. '<br>';
+            echo 'Mail: '. $reg['mail']. '<br>';
+            echo 'Curso: ';
+            switch($reg['codigocurso']){
+                case 1:
+                    echo 'PHP';
+                    break;
+                case 2:
+                    echo 'ASP';
+                    break;
+                case 3:
+                    echo 'JSP';
+                    break;
+            }
+            echo '<br>';
+            echo '<br>';
+        }
+        
         ?>
     </body>
 </html>
