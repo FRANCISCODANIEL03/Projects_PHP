ba184c3db7f9cc7db5a3eb51cf9e6f944a322607
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-22 21:45:22 -0600

    get data to the db

--- BD/pagina5.php
+++ BD/pagina5.php
@@ -7,7 +7,11 @@
         $conexion = mysqli_connect('host', 'user', 'password', 'base')
         or die('Problemas con la conexion');
 
-        
+        $registros = mysqli_query($conexion, "SELECT codigo, nombre, mail, codigocurso FROM alumnos") 
+        or die('Prolemas en el select'.mysqli_error($conexion));
+
+       
+        mysqli_close($conexion);
         ?>
     </body>
 </html>
