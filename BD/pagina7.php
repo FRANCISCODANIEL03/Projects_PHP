59968adbad05754c2df5f67cc9f099682377a410
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-23 21:49:58 -0600

    show data

--- BD/pagina7.php
+++ BD/pagina7.php
@@ -10,6 +10,27 @@
         $registros = mysqli_query($conexion, "SELECT codigo, nombre, mail, codigocurso FROM alumnos WHERE mail='$_REQUEST[mail]'") 
         or die('Prolemas en el select'.mysqli_error($conexion));
 
+        if($reg = mysqli_fetch_array($registros)){
+            echo 'Codigo: '. $reg['codigo']. '<br>';
+            echo 'Nombre: '. $reg['nombre']. '<br>';
+            echo 'Mail: '. $reg['mail']. '<br>';
+            echo 'Curso: ';
+            switch($reg['codigocurso']){
+                case 1:
+                    echo 'PHP';
+                    break;
+                case 2:
+                    echo 'ASP';
+                    break;
+                case 3:
+                    echo 'JSP';
+                    break;
+            }
+            echo '<br>';
+            echo '<br>';
+        }else{
+            echo 'No existe un alumno con ese mail ';
+        }
        
         ?>
     </body>
