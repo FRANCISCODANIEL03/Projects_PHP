520d65ea14ba684ae71ba60c337f9a38a5423ea6
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-24 21:51:06 -0600

    add inputs

--- BD/pagina8.php
+++ BD/pagina8.php
@@ -4,7 +4,9 @@
     </head>
     <body>
         <form action="pagina9.php" method="post">
-    
+            Ingrese el mail del alumno a borrar: 
+            <input type="text" name="mail"><br>
+            <input type="submit" value="buscar y borrar">
         </form>
     </body>
 </html>
