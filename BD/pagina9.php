191276f781b682f93ebbf5fab1c1aa479ddade96
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-25 21:53:05 -0600

    delete data from the db

--- BD/pagina9.php
+++ BD/pagina9.php
@@ -10,7 +10,15 @@
         $registros = mysqli_query($conexion, "SELECT * FROM alumnos WHERE mail='$_REQUEST[mail]'") 
         or die('Prolemas en el select'.mysqli_error($conexion));
 
-       
+        if($reg = mysqli_fetch_array($registros)){
+            mysqli_query($conexion , "DELETE FROM alumnos WHERE mail='$_REQUEST[mail]'")
+            or die('Problemas con el select'. mysqli_error($conexion));
+
+            echo 'Se efectuo el borrado del alumno con dicho mail';
+        }else{
+            echo 'No existe un alumno con ese mail';
+        }
+        
         ?>
     </body>
 </html>
