dee56213a793c8780493067f9af5210a86c90efb
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-26 21:54:26 -0600

    add inputs

--- BD/pagina10.php
+++ BD/pagina10.php
@@ -4,7 +4,9 @@
     </head>
     <body>
         <form action="pagina11.php" method="post">
-            
+            Ingrese el mail del alumno:
+            <input type="text" name="mail"><br>
+            <input type="submit" value="buscar">
         </form>
     </body>
 </html>
