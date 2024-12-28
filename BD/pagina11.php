30655b87f9e78fbd360c90e255a1f8c742306de3
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-27 22:00:09 -0600

    change data

--- BD/pagina11.php
+++ BD/pagina11.php
@@ -10,7 +10,20 @@
         $registros = mysqli_query($conexion, "SELECT * FROM alumnos WHERE mail='$_REQUEST[mail]'") 
         or die('Prolemas en el select'.mysqli_error($conexion));
 
-        
+        if($reg = mysqli_fetch_array($registros)){
+        ?>
+        <form action="pagina12.php" method="post">
+            Ingrese nuevo mail:
+            <input type="text" name="mailnuevo" value="<?php
+            echo $reg['mail'] ?>"><br>
+            <input type="hidden" name="mailviejo" value="<?php
+            echo $reg['mail'] ?>"><br>
+            <input type="submit" value="Modificar">
+        </form>
+        <?php
+        }else{
+            echo 'No existe un alumno con ese mail';
+        }
         ?>
     </body>
 </html>
