3573f42e8a6ebc48b2e9d03d284bd522e504137e
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-29 22:05:02 -0600

    create connection with the db

--- BD/pagina13.php
+++ BD/pagina13.php
@@ -10,7 +10,12 @@
             <input type="text" name="mail"><br>
             Seleccione el curso:
             <select name="codigocurso">
-              
+                <?php
+                $conexion = mysqli_connect('localhost', 'root', 'FRANCISCO031147', 'base1')
+                or die('Problemas con la conexion');
+          
+                
+                ?>
             </select>
             <br>
             <input type="submit" value="Registrar">
