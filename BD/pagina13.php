421957548daf4b52496b95d40bc6c007e102ef17
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-29 22:03:59 -0600

    add inputs and select

--- BD/pagina13.php
+++ BD/pagina13.php
@@ -4,7 +4,16 @@
     </head>
     <body>
         <form action="pagina14.php" method="post">
-           
+            Ingrese nombre:
+            <input type="text" name="nombre"><br>
+            Ingrese mail:
+            <input type="text" name="mail"><br>
+            Seleccione el curso:
+            <select name="codigocurso">
+              
+            </select>
+            <br>
+            <input type="submit" value="Registrar">
         </form>
     </body>
 </html>
