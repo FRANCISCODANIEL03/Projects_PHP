9d4b1d7ac64e0c8ee89df638aa7ecb4b581d0c6c
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-30 22:08:21 -0600

    create connection

--- BD/pagina14.php
+++ BD/pagina14.php
@@ -3,6 +3,12 @@
         <title>Prolema</title>
     </head>
     <body>
+        <?php
+        $conexion = mysqli_connect('host', 'user', 'password', 'base')
+        or die('Problemas con la conexion');
+
         
+        ?>
+
     </body>
 </html>
