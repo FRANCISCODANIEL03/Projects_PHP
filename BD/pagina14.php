0db09039a0ae1f58b60a90d5db9aadd5cd077d34
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-30 22:08:42 -0600

    insert new data in the db

--- BD/pagina14.php
+++ BD/pagina14.php
@@ -7,7 +7,12 @@
         $conexion = mysqli_connect('host', 'user', 'password', 'base')
         or die('Problemas con la conexion');
 
+        mysqli_query($conexion, "INSERT alumnos(nombre, mail, codigocurso) VALUES
+                    ('$_REQUEST[nombre]', '$_REQUEST[mail]', $_REQUEST[codigocurso])")
+        or die('Problemas en el select: '. mysqli_error($conexion));
+        echo 'El alumno fue dado de alta';
         
+    
         ?>
 
     </body>
