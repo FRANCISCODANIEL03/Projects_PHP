a6ba534c658ab29d127b1539e5e7a09c54e074a8
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-02 22:13:02 -0600

    add script to extra functions

--- Proyecto_final/index.php
+++ Proyecto_final/index.php
@@ -6,6 +6,28 @@
     <title>Listado Familiar</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<body>
+  
+</body>
+<script>
+    if(window.history.replaceState){
+    window.history.replaceState(null, null, window.location.href)
+}
+// Seleccionamos el botón y el div
+const boton = document.getElementById('mostrar');
+const div = document.getElementById('table_list');
 
+// Agregamos un evento de clic al botón
+boton.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (div.style.display === 'none' || div.style.display === '') {
+        div.style.display = 'grid'; // Mostrar el div
+        boton.textContent = 'Ocultar Listado'; // Cambiar texto del botón
+    } else {
+        div.style.display = 'none'; // Ocultar el div
+        boton.textContent = 'Mostrar Listado'; // Cambiar texto del botón
+    }
+});
+</script>
 </html>
 
