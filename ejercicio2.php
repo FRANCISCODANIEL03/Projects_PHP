1eba03c58188d85c040c55aaff32c49b784dc6e6
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-19 21:04:20 -0600

    add funtions php

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,5 +1,23 @@
 <html>
     <body>
-   
+    <?php
+        $edad = 20;
+        $peso = 90.25;
+        $nombre = 'juan';   
+        $existe = true;
+        echo 'Variable integer: ';
+        echo $edad;
+        echo '<br>';
+        echo 'Variable double: ';
+        echo $peso;
+        echo '<br>';
+        echo 'Variable string: ';
+        echo $nombre.'<br>';
+        echo 'Concatenar: ';
+        echo $nombre.''.$nombre;
+        echo '<br>';
+        echo 'Variable boolean: ';
+        echo $existe;
+    ?>
     </body>
 </html>
