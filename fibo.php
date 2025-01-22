5f970f8bcbd08823730a698832a7d0e67fa1e268
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-21 21:07:52 -0600

    add functions php

--- fibo.php
+++ fibo.php
@@ -4,6 +4,18 @@
     </head>
 
     <body>
-        
+        <?php
+            $num = $_REQUEST['num'];
+            if($num == NULL) echo 'Valor no enviado';
+            if(!is_numeric($num)) echo 'Error de datos enviados';
+            else{    
+            function fibo($num1){
+                if($num1 < 2){return $num1;}
+                else{return fibo($num1-1) + fibo($num1 - 2);}
+            }
+            $res = fibo($num);
+            echo $res;
+            }
+        ?>
     </body>
 </html>
