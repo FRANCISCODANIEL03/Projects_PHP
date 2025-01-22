b9bdbeac5cb32e9ce17456ef03585a0ae49232ca
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-21 21:06:44 -0600

    add cycle while

--- ejercicio4.php
+++ ejercicio4.php
@@ -9,7 +9,13 @@
             for($f = 1; $f <= 100; $f++){
                 echo $f.', ';
             }
-            
+            # Ciclo while
+            $valor = rand(1, 100);
+            $inicio = 1;
+            while($inicio <= $valor){
+                echo $inicio.'<br>',
+                $inicio++;
+            }
         ?>
     </body>
 </html>
