93678bdab4e4e4f4842b63a2b064fa34eea8d53d
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-22 21:09:28 -0600

    add inputs

--- fibonacci.php
+++ fibonacci.php
@@ -5,6 +5,10 @@
 
     <body>
     <form action="fibo.php" method="post">
-           
+            Ingresa el limite:
+            <input type="text" name="num">
+            <br>
+            <input type="submit" name="operar">
+        </form>
     </body>
 </html>
