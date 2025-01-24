0fce0eeed1d59c495ccb957d6fc5a8bcba94a88a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-23 21:11:44 -0600

    add inputs

--- imc.php
+++ imc.php
@@ -5,7 +5,13 @@
 
     <body>
     <form action="pagina3.php" method="post">
-            
+            Ingresa tu peso:
+            <input type="text" name="peso">
+            <br>
+            Ingresa tu estatura:
+            <input type="text" name="estatura">
+            <br>
+            <input type="submit" name="operar">
         </form>
     </body>
 </html>
