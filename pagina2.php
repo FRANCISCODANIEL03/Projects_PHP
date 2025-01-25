9f3ac6e459e4645ad5dc46a2dcff324a250a0c88
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-24 21:12:44 -0600

    add "if" structure

--- pagina2.php
+++ pagina2.php
@@ -4,6 +4,14 @@
     </head>
 
     <body>
-        
+        <?php
+            if($_REQUEST['radio1'] == 'suma'){
+                $suma = $_REQUEST['valor1'] + $_REQUEST['valor2'];
+                echo 'la suma es '.$suma;
+            }elseif($_REQUEST['radio1'] == 'resta'){
+                $resta = $_REQUEST['valor1'] - $_REQUEST['valor2'];
+                echo 'La resta es: '.$resta;
+            }
+        ?>
     </body>
 </html>
