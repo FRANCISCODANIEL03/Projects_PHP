a51d4cd217b54f0626b2244f86041a0dd911ad98
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-25 21:13:50 -0600

    add functions php

--- pagina3.php
+++ pagina3.php
@@ -4,6 +4,9 @@
     </head>
 
     <body>
-        
+        <?php
+            $imc = $_REQUEST['peso'] / ($_REQUEST['estatura']*$_REQUEST['estatura']);
+            echo 'Tu IMC es: '.$imc;
+        ?>
     </body>
 </html>
