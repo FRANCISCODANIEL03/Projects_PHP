4fd2861d48978a6e38d6f60a0fae3f719f8b04c4
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-25 21:16:35 -0600

    add select and inputs

--- BD/pagina1.php
+++ BD/pagina1.php
@@ -5,7 +5,18 @@
     <body>
         <h1>Alta de alumno</h1>
         <form action="pagina2.php" method="post">
-           
+            Ingrese nombre:
+            <input type="text" name="nombre"><br>
+            Ingrese mail: 
+            <input type="text" name="mail"><br>
+            Selecciona el curso:
+            <select name="codigocurso">
+                <option value="1">PHP</option>
+                <option value="2">ASP</option>
+                <option value="3">JSP</option>
+            </select>
+            <br>
+            <input type="submit" value="Registrar">
         </form>
     </body>
 </html>
