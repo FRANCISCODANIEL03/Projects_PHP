0433f6b4d4f99275d4804c11ee188c54086a85b1
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-25 21:15:46 -0600

    add form

--- BD/pagina1.php
+++ BD/pagina1.php
@@ -3,6 +3,9 @@
         <title>Problema</title>
     </head>
     <body>
-       
+        <h1>Alta de alumno</h1>
+        <form action="pagina2.php" method="post">
+           
+        </form>
     </body>
 </html>
