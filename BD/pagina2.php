3b448b17f13396706e0feb00a61e5334a3280faa
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-26 21:32:59 -0600

    add functions php

--- BD/pagina2.php
+++ BD/pagina2.php
@@ -4,6 +4,16 @@
     </head>
     <body>
         <?php
-            
+            $conexion = mysqli_connect('host', 'user', 'password', 'base')
+            or die('Problemas con la conexion');
+
+            mysqli_query($conexion, "INSERT alumnos
+            (nombre, mail, codigocurso) VALUES
+            ('$_REQUEST[nombre]', '$_REQUEST[mail]', $_REQUEST[codigocurso])")
+            or die('Problemas en el INSERT'.mysqli_error($conexion));
+
+            mysqli_close($conexion);
+            echo 'El alumno fue dado de alta';
+            ?>
     </body>
 </html>
